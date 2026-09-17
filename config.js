3e2358c0660bcfb17946f0b2423a253ef9375c06
BNDSIR <bholanath891@gmail.com>
2026-09-17 20:01:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "YOUR-PUBLISHABLE-OR-ANON-KEY",
+  SUPABASE_URL: "https://lakxtousjfjvluygpbnc.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_W3gLK9DSEjK0R_gJucs6RA_o-VbMe41",
   SCHOOL_LOGO: "logo.png",
   REFRESH_SECONDS: 20
 };
